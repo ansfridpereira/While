97f86ceb182de8b05ce7534dde92dc57d4459749
ansfridpereira <89774958+ansfridpereira@users.noreply.github.com>
2021-09-18 06:49:36 +0530

    While loop

---
+++ src/While.java
@@ -0,0 +1,10 @@
+class While {
+    public static void main(String[] args)
+    {
+        int x=1;
+        for(x=x+1;x%10!=1;x=x+2)
+        {
+            System.out.println("x =" + x);
+        }
+    }
+}
